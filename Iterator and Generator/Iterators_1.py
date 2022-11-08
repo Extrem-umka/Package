e6586ca52c95be54817737117daee1830c30c479
Extrem_umka <zhulianev@gmail.com>
2022-11-09 01:17:37 +0600

    Change9

--- Iterator and Generator/Iterators_1.py
+++ Iterator and Generator/Iterators_1.py
@@ -2,7 +2,7 @@
 # их плоское представление, т.е последовательность состоящую из вложенных элементов. Функция test в коде ниже также
 # должна отработать без ошибок.
 
-list_of_list = [
+list_of_lists = [
     ['a', 'b', 'c'],
     ['d', 'e', 'f', 'h', False],
     [1, 2, None]
@@ -11,23 +11,28 @@ list_of_list = [
 class FlatIterator:
 
     def __init__(self, list_of_list):
-        self.start = -1
-        self.end = len(list_of_list)
-
+        self.list_len = len(list_of_list)
+        self.list_of_list = list_of_list
 
     def __iter__(self):
+        self.outer_list_cursor = 0
+        self.inner_list_cursor = 0
         return self
 
     def __next__(self):
-        self.start += 1
-        if self.start == self.end:
+        self.inner_list_cursor += 1
+        if self.inner_list_cursor == len(self.list_of_list[self.outer_list_cursor]):
+            iter(self)
+            self.outer_list_cursor += 1
+        if self.outer_list_cursor == self.list_len:
             raise StopIteration
-        return ' '.join(str(elem) for elem in list_of_list[self.start])
+            self.inner_list_cursor += 1
+        return self.list_of_list[self.outer_list_cursor][self.inner_list_cursor]
 
 
-for item in FlatIterator(list_of_list):
+for item in FlatIterator(list_of_lists):
     print(item)
-
+#
 def test():
 
     list_of_lists_1 = [
